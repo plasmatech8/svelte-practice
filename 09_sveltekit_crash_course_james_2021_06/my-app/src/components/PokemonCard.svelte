6338c7447256e2a7f80e7f5f01d00822f7c83f8c
plasmatech8 <com0004@gmail.com>
2021-11-14 21:20:55 +1100

    Add fade in transition

--- 09_sveltekit_crash_course_james_2021_06/my-app/src/components/PokemonCard.svelte
+++ 09_sveltekit_crash_course_james_2021_06/my-app/src/components/PokemonCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import type { PokemanType } from 'src/stores/pokestore';
 	export let pokeman: PokemanType;
 </script>
@@ -6,6 +7,7 @@
 <a
 	class="p-6 bg-gray-100 text-gray-800 text-center shadow-sm hover:shadow-md flex flex-col items-center sadsa"
 	href="/pokemon/{pokeman.id}"
+	in:fade={{ duration: 200 }}
 >
 	<img class="h-40 w-40" src={pokeman.image} alt={pokeman.name} />
 	<h3 class="uppercase text-2xl">{pokeman.id}. {pokeman.name}</h3>
